5b61ec83edbd79efc543346621b5eae30aadef03
matthew-bettingcloud <matthew@bettingcloud.com>
2017-10-05 09:37:44 +0200

    chore: added stylelint ordering. Added commitlint to help validate correctly git messages.

---
+++ mang-ngx-ui-skeleton/stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: "stylelint-config-standard",
+    plugins: [
+        "stylelint-order"
+    ],
+    rules: {
+        "order/order": [
+            "custom-properties",
+            "declarations"
+        ],
+        "order/properties-alphabetical-order": true,
+        "selector-type-no-unknown": [
+            true,
+            {
+                "ignoreTypes": [
+                    "/^mm-/"
+                ]
+            }
+        ]
+    }
+
+}
